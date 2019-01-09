f9a9febf524a61347d0b4621f59297ab4b21b195
michaelalanrll <michael.alan@ralali.com>
2019-01-09 14:27:47 +0700

    Add view data

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,16 @@
 @extends('master')
 @section('content')
-  Index page
+  <div class="container">
+    <div class="content">
+      @foreach($data as $d)
+      <div class="panel panel-default">
+        <div class="panel-heading">$d->title</div>
+        <div class="panel-body">
+          $d->content
+        </div>
+        <div class="panel-footer">$d->date</div>
+      </div>
+      @endforeach
+    </div>
+  </div>
 @endsection
